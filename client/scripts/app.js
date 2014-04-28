2948e177a670ff93e0d78f08308f6b4036c186e7
mcreinhard <mcreinhard@gmail.com>
2014-04-28 15:09:21 -0700

    Implemented message fetching and refreshing display

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,65 @@
 // YOUR CODE HERE:
 
+var app = {};
+
+(function() {
+  app.init = function() {
+    setInterval(app.fetch, 100);
+  };
+
+  app.send = function(message) {
+    $.ajax({
+      url: app.server,
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        console.error('chatterbox: Failed to send message');
+      }
+    });
+  };
+
+
+  app.fetch = function() {
+    if (!app.fetch.inProgress) {
+      app.fetch.inProgress = true;
+      $.ajax(app.server, {
+        type: 'GET',
+        //data: JSON.stringify(message),
+        contentType: 'application/json',
+        success: function (data) {
+          app.fetch.inProgress = false;
+          $('#chats').empty();
+          _.each(data.results, function(result) {
+            app.addMessage(result);
+          });
+        },
+        error: function (data) {
+          app.fetch.inProgress = false;
+          console.error('chatterbox: Failed to retrieve message');
+        }
+      });
+    }
+  };
+  app.fetch.inProgress = false;
+
+  app.clearMessages = function() {
+    $('#chats').empty();
+  };
+
+  app.addMessage = function(message) {
+    var $message = $('<div></div>');
+    $message.text(message.username + ': ' + message.text);
+    $('#chats').append($message);
+
+  };
+
+  app.addRoom = function(roomName) {
+    $('#roomSelect').append('<a href="#">' + roomName + '</a>');
+  } ;
+
+  app.server = 'https://api.parse.com/1/classes/chatterbox';
+}).call(this);
